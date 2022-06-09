25451a2f361732016e2014113428607cdbed5fc1
cchadsey <cha22006@byui.edu>
2022-06-08 22:55:54 -0500

    merging changes

--- greed/__main__.py
+++ greed/__main__.py
@@ -2,8 +2,8 @@ import os
 import random
 
 from game.casting.actor import Actor
-from game.casting.fallingObject import FallingObject
 from game.casting.cast import Cast
+from game.casting.fallingObject import FallingObject
 
 from game.directing.director import Director
 
@@ -12,7 +12,6 @@ from game.services.video_service import VideoService
 
 from game.shared.color import Color
 from game.shared.point import Point
-from game.casting.fallingObject import FallingObject
 
 
 FRAME_RATE = 12
@@ -23,22 +22,16 @@ FONT_SIZE = 15
 COLS = 60
 ROWS = 40
 CAPTION = "Greed"
-DATA_PATH = os.path.dirname(os.path.abspath(__file__)) + "/data/messages.txt"
-WHITE = Color(255, 255, 255)
-DEFAULT_ROCKS = 20
-DEFAULT_GEMS = 20
-
-CAPTION = "Greed"
 WHITE = Color(255, 255, 255)
 DEFAULT_GEMS = 20
 DEFAULT_ROCKS = 20
 
 
 def main():
-    
+
     # create the cast
     cast = Cast()
-    
+
     # create the banner
     banner = Actor()
     banner.set_text("The Score")
@@ -46,14 +39,6 @@ def main():
     banner.set_color(WHITE)
     banner.set_position(Point(CELL_SIZE, 0))
     cast.add_actor("banners", banner)
-    
-    # create the player
-    x = int(MAX_X / 2)
-    y = int(MAX_Y - 20)
-    position = Point(x, y)
-
-    player = Actor()
-    player.set_text("#")
 
     #create  player
     x = int(MAX_X/ 2)
@@ -67,50 +52,6 @@ def main():
     player.set_position(position)
     cast.add_actor("player", player)
 
-    
-    # create the falling gem
-
-    for n in range(DEFAULT_GEMS):
-        x = random.randint(0, COLS-1)
-        y = random.randint(1, ROWS-1)
-        position = Point(x,y)
-        position = position.scale(CELL_SIZE)
-
-        r= random.randint(0,225)
-        g= random.randint(0,225)
-        b= random.randint(0,225)
-        color = Color(r,g,b)
-
-        gem = FallingObject()
-        gem.set_text("*")
-        gem.set_font_size(FONT_SIZE)
-        gem.set_color(color)
-        gem.set_position(position)
-        cast.add_actor("gems", gem)
-
-#add falling rock
-    for n in range(DEFAULT_ROCKS):
-        x = random.randint(0, COLS-1)
-        y = random.randint(1, ROWS-1)
-        position = Point(x,y)
-        position = position.scale(CELL_SIZE)
-
-        r= random.randint(0,225)
-        g= random.randint(0,225)
-        b= random.randint(0,225)
-        color = Color(r,g,b)
-
-        rock = FallingObject()
-        rock.set_text("O")
-        rock.set_font_size(FONT_SIZE)
-        rock.set_color(color)
-        rock.set_position(position)
-        cast.add_actor("rocks", rock)
-
-
-    
-
-
     #create Gems
     for n in range(DEFAULT_GEMS):
         x=random.randint(1, COLS-1)
@@ -123,12 +64,12 @@ def main():
         b = random.randint(0, 255)
         color = Color(r, g, b)
         
-    gem=Actor()
-    gem.set_text('*')
-    gem.set_font_size(FONT_SIZE)
-    gem.set_color(color)
-    gem.set_position(position)
-    cast.add_actor("falling_objects", gem)
+        gem=Actor()
+        gem.set_text('*')
+        gem.set_font_size(FONT_SIZE)
+        gem.set_color(color)
+        gem.set_position(position)
+        cast.add_actor("falling_objects", gem)
 
 
     #create Rocks
@@ -147,7 +88,16 @@ def main():
         rock.set_position(position)
         cast.add_actor("falling_objects", rock)
 
+'''THESE NEED TO BE SET TO THE GEM ACTORS
+    #create random gem_rock color ()
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    color = Color(r, g, b)
 
+    #set volocity
+    velocity = Point(0, 1)
+    THESE NEED TO BE SET TO THE GEM ACTORS'''
 
     # start the game
     keyboard_service = KeyboardService(CELL_SIZE)
@@ -156,5 +106,5 @@ def main():
     director.start_game(cast)
 
 
-    if __name__ == "__main__":
-        main()
+if __name__ == "__main__":
+    main()
